31db5064a62cddd27b1e709dfe9dd1853a1f627a
Kendrik <57840931+NoNameSpecified@users.noreply.github.com>
2025-06-11 04:13:01 +0200

    Create __init__.py

---
+++ src/game_libs/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+INFO for Skender Bot:
+
+	this is just to inform python that the files here are supposed to be imported
+
+	leave it, even if it's empty !!!!
+
+	Official Repo: https://github.com/NoNameSpecified/UnbelievaBoat-Python-Bot
+
+"""
